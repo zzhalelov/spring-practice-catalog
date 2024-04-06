5edca67be7e35b86af9213483ff70a069440f1e0
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-04-06 08:55:16 +0500

    feat: create class MainController

---
+++ src/main/java/kz/runtime/spring_practice_catalog/controller/MainController.java
@@ -0,0 +1,12 @@
+package kz.runtime.spring_practice_catalog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public String mainPage() {
+        return "index";
+    }
+}
