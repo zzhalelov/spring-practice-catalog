1127ed418c42c2bd5fcf232c8d962bd099e2369c
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-04-09 23:48:55 +0500

    feat: add annotations @Entity, @Table, @Id, @GeneratedValue

--- src/main/java/kz/runtime/spring_practice_catalog/model/Product.java
+++ src/main/java/kz/runtime/spring_practice_catalog/model/Product.java
@@ -1,16 +1,24 @@
 package kz.runtime.spring_practice_catalog.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.ManyToAny;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
     String name;
     double price;
+
+    @ManyToOne
     Category category;
 }
