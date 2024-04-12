53c6dc596918bd936bac954f3f946ee0f85ffd0b
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-04-13 00:34:53 +0500

    feat: add tables options & values

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,10 +3,26 @@ CREATE TABLE categories
     id   SERIAL PRIMARY KEY,
     name VARCHAR
 );
+
 CREATE TABLE products
 (
     id          SERIAL PRIMARY KEY,
     name        VARCHAR,
     price       INT,
     category_id INT REFERENCES categories (id)
+);
+
+CREATE TABLE options
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR,
+    category_id INT REFERENCES categories (id)
+);
+
+CREATE TABLE values
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR,
+    product_id INT REFERENCES products (id),
+    option_id  INT REFERENCES options (id)
 );
