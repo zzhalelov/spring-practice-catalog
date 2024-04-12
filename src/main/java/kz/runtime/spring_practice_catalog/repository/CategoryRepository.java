0a15bf5045348f9141ab3f3a8af3f99f226ff6f2
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-04-12 21:59:16 +0500

    fix: clear code in CategoryRepository & ProductRepository

--- src/main/java/kz/runtime/spring_practice_catalog/repository/CategoryRepository.java
+++ src/main/java/kz/runtime/spring_practice_catalog/repository/CategoryRepository.java
@@ -1,40 +1,10 @@
 package kz.runtime.spring_practice_catalog.repository;
 
 import kz.runtime.spring_practice_catalog.model.Category;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-@Component
-public class CategoryRepository implements SimpleRepository<Category> {
-    private final Map<Long, Category> categories;
-
-    private static long serial = 1;
-
-    public CategoryRepository() {
-        this.categories = new HashMap<>();
-    }
-
-    @Override
-    public void create(Category category) {
-        category.setId(getUniqueId());
-        categories.put(category.getId(), category);
-    }
-
-    @Override
-    public Category findById(long id) {
-        return categories.get(id);
-    }
-
-    @Override
-    public List<Category> findAll() {
-        return new ArrayList<>(categories.values());
-    }
-
-    private long getUniqueId() {
-        return serial++;
-    }
 }
--- src/main/java/kz/runtime/spring_practice_catalog/repository/ProductRepository.java
+++ src/main/java/kz/runtime/spring_practice_catalog/repository/ProductRepository.java
@@ -1,40 +1,11 @@
 package kz.runtime.spring_practice_catalog.repository;
 
 import kz.runtime.spring_practice_catalog.model.Product;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-@Component
-public class ProductRepository implements SimpleRepository<Product> {
-    private final Map<Long, Product> products;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    private static long serial = 1;
-
-    public ProductRepository() {
-        this.products = new HashMap<>();
-    }
-
-    @Override
-    public void create(Product product) {
-        product.setId(getUniqueId());
-        products.put(product.getId(), product);
-    }
-
-    @Override
-    public Product findById(long id) {
-        return products.get(id);
-    }
-
-    @Override
-    public List<Product> findAll() {
-        return new ArrayList<>(products.values());
-    }
-
-    private long getUniqueId() {
-        return serial++;
-    }
 }
