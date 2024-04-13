566d3d979a6ee4b0cab7daec7ca5c3cbfaa83dc8
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-04-13 09:00:20 +0500

    feat: create ValueRepository

---
+++ src/main/java/kz/runtime/spring_practice_catalog/repository/ValueRepository.java
@@ -0,0 +1,8 @@
+package kz.runtime.spring_practice_catalog.repository;
+
+import kz.runtime.spring_practice_catalog.model.Value;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ValueRepository extends JpaRepository<Value, Long> {
+
+}
