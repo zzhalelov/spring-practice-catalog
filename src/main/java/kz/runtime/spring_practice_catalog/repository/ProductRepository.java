dd5c9d9db444f5ac5d4cca9d47059ea839609198
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-05-21 13:25:30 +0500

    fix: add method findByPriceBetween in ProductRepository

--- src/main/java/kz/runtime/spring_practice_catalog/repository/ProductRepository.java
+++ src/main/java/kz/runtime/spring_practice_catalog/repository/ProductRepository.java
@@ -4,8 +4,10 @@ import kz.runtime.spring_practice_catalog.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    List<Product> findByPriceBetween(Double minPrice, Double maxPrice);
 }
