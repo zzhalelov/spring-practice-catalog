c5f342c37120eeaa513c814d64b4d50fc20326bf
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-05-21 13:51:12 +0500

    feat: add in sql tables: users, orders, reviews, order_products

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -26,3 +26,42 @@ CREATE TABLE values
     product_id INT REFERENCES products (id),
     option_id  INT REFERENCES options (id)
 );
+
+--Создание таблицы пользователей
+CREATE TABLE users
+(
+    id            BIGSERIAL PRIMARY KEY,
+    login         VARCHAR NOT NULL UNIQUE,
+    password      VARCHAR NOT NULL,
+    name          VARCHAR,
+    lastname      VARCHAR,
+    role          int,
+    registered_at timestamp
+);
+
+CREATE TABLE orders
+(
+    id               BIGSERIAL PRIMARY KEY,
+    user_id          BIGINT REFERENCES users (id) NOT NULL,
+    status           INT,
+    delivery_address VARCHAR                      NOT NULL,
+    date_of_order    TIMESTAMP
+);
+
+CREATE TABLE reviews
+(
+    id            BIGSERIAL PRIMARY KEY,
+    user_id       BIGINT REFERENCES users (id)    NOT NULL,
+    product_id    BIGINT REFERENCES products (id) NOT NULL,
+    review_rating SMALLINT                        NOT NULL,
+    review_text   TEXT,
+    review_data   TIMESTAMP
+);
+
+CREATE TABLE orders_products
+(
+    id         BIGSERIAL PRIMARY KEY,
+    order_id   BIGINT REFERENCES orders (id),
+    product_id BIGINT REFERENCES products (id),
+    quantity   INT NOT NULL
+);
